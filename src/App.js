e3abf7719f0352fb6ec404e6a1ff118e3a6ffa13
SeymaDemir <seymademir35@gmail.com>
2023-02-20 18:39:21 +0300

    signup page raw codes ready

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import About from '../src/ComponentsFolders/About/About';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import About from "../src/ComponentsFolders/About/About";
 import Blog from "../src/ComponentsFolders/Blog/Blog";
 import BlogDetail from "../src/ComponentsFolders/BlogDetail/BlogDetail";
 import Calender from "../src/ComponentsFolders/Calender/Calender";
@@ -15,34 +17,59 @@ import Meetings from "../src/ComponentsFolders/Meeting/Meetings";
 import MemberShip from "../src/ComponentsFolders/Membership/MemberShip";
 import Navbar from "../src/ComponentsFolders/Navbar/Navbar";
 import Search from "../src/ComponentsFolders/Search/Search";
-import Sidebar from "../src/ComponentsFolders/Sidebar/Sidebar";
+// import Sidebar from "../src/ComponentsFolders/Sidebar/Sidebar";
 import SignUp from "../src/ComponentsFolders/SignUp/SignUp";
 import UserCourseContent from "../src/ComponentsFolders/UserCourseContent/UserCourseContent";
 
-
 function App() {
   return (
+    // <BrowserRouter>
+    //   <Navbar />
+    //   <Routes>
+    //   <Route exact path="/" element={<HomePage />} />
+    //   <Route path="/about" element={<About />} />
+    //   <Route path="/blog" element={<Blog />} />
+    //   <Route path="/blogdetail" element={<BlogDetail />} />
+    //   <Route path="/calender" element={<Calender />} />
+    //   <Route path="/checkout" element={<CheckOut />} />
+    //   <Route path="/coursecategory" element={<CourseCategory />} />
+    //   <Route path="/coursecontent" element={<CourseContent />} />
+    //   <Route path="/coursedetail" element={<CourseDetail />} />
+    //   <Route path="/courses" element={<Courses />} />
+    //   <Route path="/events" element={<Events />} />
+    //   <Route path="/login" element={<Login />} />
+    //   <Route path="/meetings" element={<Meetings />} />
+
+    //     <Meetings />
+    //     <MemberShip />
+
+    //     <Search />
+    //     <SignUp />
+    //     <UserCourseContent />
+    //   </Routes>
+    //   <Footer />
+    // </BrowserRouter>
     <>
-      <About/>
-      <Blog/>
-      <BlogDetail/>
-      <Calender/>
-      <CheckOut/>
-      <CourseCategory/>
-      <CourseContent/>
-      <CourseDetail/>
-      <Courses/>
-      <Events/>
-      <Footer/>
-      <HomePage/>
-      <Login/>
-      <Meetings/>
-      <MemberShip/>
-      <Navbar/>
-      <Search/>
-      <Sidebar/>
-      <SignUp/>
-      <UserCourseContent/>
+      <About />
+      <Blog />
+      <BlogDetail />
+      <Calender />
+      <CheckOut />
+      <CourseCategory />
+      <CourseContent />
+      <CourseDetail />
+      <Courses />
+      <Events />
+      <Footer />
+      <HomePage />
+      <Login />
+      <Meetings />
+      <MemberShip />
+      <Navbar />
+      <Search />
+      {/* <Sidebar/> */}
+      <SignUp />
+      <UserCourseContent />
     </>
   );
 }
--- src/ComponentsFolders/SignUp/SignUp.jsx
+++ src/ComponentsFolders/SignUp/SignUp.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import registerImage from "./Images/registerImg.svg"
 
 const SignUp = () => {
   return (
-    <div>
-      
+    <div className='flex lg:flex-row flex-col w-full font-poppins'>
+      <div className='pl-10 pt-10 pr-10 basis-1/2 flex '>
+        <img src={registerImage} alt="registerImg" />
+      </div>
+      <div className='pl-10 pt-10 pr-10 flex flex-col justify-center text-center basis-1/2 '>
+        <h3>Welcome to lorem!</h3>
+        <div className='mt-6 flex flex-row self-center text-center bg-[#92D6D6] p-2 text-white justify-center rounded-full w-96'>
+          <div className='basis-1/2 self-center'>Login</div>
+          <div className='basis-1/2 bg-[#49BBBD] rounded-full p-2'>Register</div>
+        </div>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        <div>form input</div>
+        <div>register button</div>
+      </div>
     </div>
   )
 }
