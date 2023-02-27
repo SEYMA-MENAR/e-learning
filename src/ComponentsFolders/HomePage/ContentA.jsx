6d68e2cb9ef8a9b6469550073dc89f977c423599
Menar Selamet <menarselamet007@gmail.com>
2023-02-27 11:19:46 +0300

    Backgroud color added

--- src/ComponentsFolders/HomePage/ContentA.jsx
+++ src/ComponentsFolders/HomePage/ContentA.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from 'react';
+import Background from "./Images/Background.svg";
+import Girl from './Images/Girl.svg';
+
 
 const ContentA = () => {
   return (
-    <div>
-      
+    <div
+      style={{
+        backgroundImage: `url(${Background})`,
+        height: "100vh",
+        width: "100vw",
+        backgroundSize: "cover",
+        backgroundPosition: "top",
+        backgroundRepeat: "no-repeat",
+        top: 0,
+      }}
+    >
+      <div>
+        <div>
+        <p> Studying Online is now much easier</p>
+        <p> TOTC is an interesting platfrom that will teach you in more an interactive way</p>
+        <div>
+          <button>Join for free</button>
+          <button><img src="" alt="" /> Watch how it works </button>
+
+        </div>
+        </div>
+        <div>
+          <img src={Girl} alt="" />
+
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ContentA
